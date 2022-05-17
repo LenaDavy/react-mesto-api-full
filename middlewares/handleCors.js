d7e5694cd96e8623bf6c61c3b813eff100b8ca02
Lena Davy <79175002880@ya.ru>
2022-05-17 21:42:17 +0300

    server

--- middlewares/handleCors.js
+++ middlewares/handleCors.js
@@ -1,6 +1,10 @@
 const allowedCors = [
+  'mestoru.nomoredomains.xy',
+  'www.mestoru.nomoredomains.xy',
   'http://mestoru.nomoredomains.xyz/',
-  'http://mestoru.nomoredomains.xyz/',
+  'http://www.mestoru.nomoredomains.xyz/',
+  'https://mestoru.nomoredomains.xyz/',
+  'https://www.mestoru.nomoredomains.xyz/',
   'localhost:3000',
   'http://localhost:3000/',
 ];
@@ -13,9 +17,12 @@ const handleCors = (req, res, next) => {
 
   const { method } = req;
   const DEFAULT_ALLOWED_METHODS = "GET,HEAD,PUT,PATCH,POST,DELETE";
+  const requestHeaders = req.headers['access-control-request-headers'];
 
   if (method === 'OPTIONS') {
     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+    res.header('Access-Control-Allow-Headers', requestHeaders);
+    return res.end();
  }
 
   next();
