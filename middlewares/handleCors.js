238b08cb53435e8c3b768310d26b4472aa5da27f
Lena Davy <79175002880@ya.ru>
2022-05-18 11:01:20 +0300

    server

--- middlewares/handleCors.js
+++ middlewares/handleCors.js
@@ -12,16 +12,14 @@ const handleCors = (req, res, next) => {
 
   if (allowedCors.includes(origin)) {
     res.header('Access-Control-Allow-Origin', origin);
-    res.header('Access-Control-Allow-Headers', requestHeaders);
     res.header('Access-Control-Allow-Credentials', true);
-    return res.end();
   }
 
   if (method === 'OPTIONS') {
     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
     res.header('Access-Control-Allow-Headers', requestHeaders);
     res.header('Access-Control-Allow-Credentials', true);
-    return res.end();
+    res.end();
  }
 
   return next();
