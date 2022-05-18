db93d1fda3d9f5d564ec2566f504914a65cdddb0
Lena Davy <79175002880@ya.ru>
2022-05-18 12:07:30 +0300

    server

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -6,13 +6,13 @@ const { NODE_ENV, JWT_SECRET } = process.env;
 
 module.exports = (req, res, next) => {
   const token = req.cookies.jwt;
-  if (token === null) {
+  if (!token) {
     return next(new Unauthorized('Необходима авторизация'));
   } let payload;
   try {
     payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret');
   } catch (err) {
-    return next(NotFoundError('Объект не найден'));
+    return next(new Unauthorized('Необходима авторизация'));
   } req.user = payload;
-  return next();
+ return next();
 };
