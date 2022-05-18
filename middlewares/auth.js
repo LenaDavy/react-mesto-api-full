ae0980b0ef4674f248f3d679d405b3102246aa67
Lena Davy <79175002880@ya.ru>
2022-05-18 22:37:48 +0300

    server

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -5,10 +5,10 @@ const Unauthorized = require('../errors/Unauthorized');
 const { NODE_ENV, JWT_SECRET } = process.env;
 
 module.exports = (req, res, next) => {
-  let { token } = req.headers;
-    if (!token || !token.startsWith('Bearer ')) {
+  let { Authorization } = req.headers;
+    if (!Authorization || !Authorization.startsWith('Bearer ')) {
       return next(new Unauthorized('Необходима авторизация'));
-    } token = jwt.replace('Bearer ', '');
+    } token = Authorization.replace('Bearer ', '');
       try {
         token = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret');
       } catch (err) {
