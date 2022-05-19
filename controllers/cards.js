0ac347d39f3f9d92502ec25143e241c6232155d6
Lena Davy <79175002880@ya.ru>
2022-05-19 15:22:08 +0300

    server

--- controllers/cards.js
+++ controllers/cards.js
@@ -4,8 +4,8 @@ const NotFoundError = require('../errors/NotFoundError');
 const Forbidden = require('../errors/Forbidden');
 
 module.exports.createCard = (req, res, next) => {
-  const { name, link, _id } = req.body;
-  Card.create({ name, link, _id })
+  const { name, link } = req.body;
+  Card.create({ name, link })
     .then((newCard) => {
       if (!newCard) {
         return next(new NotFoundError('Объект не найден'));
