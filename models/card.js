795bbf63aabc996df970244011434303e9b4d017
Lena Davy <79175002880@ya.ru>
2022-05-21 12:11:32 +0300

    server

--- models/card.js
+++ models/card.js
@@ -14,7 +14,8 @@ const cardSchema = new mongoose.Schema({
     required: true,
   },
   owner: {
-    type: 'ObjectId',
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'user',
   },
 
   likes: [{
