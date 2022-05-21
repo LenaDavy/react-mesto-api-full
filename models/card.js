579fbf4322ca8ed44ae079c39867a91ae37669b2
Lena Davy <79175002880@ya.ru>
2022-05-21 10:23:38 +0300

    server

--- models/card.js
+++ models/card.js
@@ -1,6 +1,31 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
-const User = require('./user');
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    minlength: 2,
+    maxlength: 30,
+    default: 'Жак-Ив Кусто',
+  },
+  about: {
+    type: String,
+    minlength: 2,
+    maxlength: 30,
+    default: 'Исследователь',
+  },
+  avatar: {
+    type: String,
+    validate: { validator: (v) => validator.isURL(v) },
+    default: 'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
+  },
+  email: {
+    type: String,
+  },
+  password: {
+    type: String,
+  },
+});
 
 const cardSchema = new mongoose.Schema({
   name: {
@@ -16,12 +41,12 @@ const cardSchema = new mongoose.Schema({
   },
   owner: {
     type: mongoose.Schema.Types.ObjectId,
-    ref: User,
+    ref: 'User',
   },
 
   likes: [{
     type: mongoose.Schema.Types.ObjectId,
-    ref: User,
+    ref: 'User',
     default: {},
   }],
   createdAt: {
@@ -29,4 +54,6 @@ const cardSchema = new mongoose.Schema({
     default: Date.now,
   },
 });
+
+module.exports = mongoose.model('User', userSchema);
 module.exports = mongoose.model('Card', cardSchema);
