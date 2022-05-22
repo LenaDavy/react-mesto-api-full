8f2ad9c3736e27d71a3f6088dcb61adfb223b49c
Lena Davy <79175002880@ya.ru>
2022-05-22 09:11:41 +0300

    server

--- controllers/cards.js
+++ controllers/cards.js
@@ -60,7 +60,7 @@ module.exports.putCardLike = (req, res, next) => {
 module.exports.deleteCardLike = (req, res, next) => {
   Card.findByIdAndUpdate(
     req.params.cardId,
-    { $pull: { likes: req.owner } },
+    { $pull: { likes: req.user._id } },
     { new: true },
   )
     .then((cards) => {
